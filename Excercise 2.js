f6497a48db502555b3768e8918377efecd6d4ea1
byidskou56 <142067633+byidskou56@users.noreply.github.com>
2023-08-12 14:35:37 -0400

    Create Excercise 2.js

---
+++ Excercise 2.js
@@ -0,0 +1,9 @@
+//This is Excercise 2 of 16 PreWork BYI 12Aug23 14:29EST
+/*
+console.log("There once was a ship that was put to see")
+console.log("Brandon Yurie was the name that be")
+console.log("When told were to go")
+console.log("It was 'Eagle Corps' where he found")
+console.log("Would be his life turning tides")
+*/
+//console.log("Hello World!")
