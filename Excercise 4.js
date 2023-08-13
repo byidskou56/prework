b23450e58739ab4a66d13371bf4a66731ee56d42
byidskou56 <142067633+byidskou56@users.noreply.github.com>
2023-08-13 18:25:45 -0400

    Successful Completion of Exercise 4

---
+++ Excercise 4.js
@@ -0,0 +1,39 @@
+//This is Excercise 4 of 16 PreWork BYI 13Aug23  EST
+console.log("5 + 4 = ");
+console.log(5 + 4);
+
+console.log("10 - 7 = ");
+console.log(10 - 7);
+
+console.log("6 * 3 = ");
+console.log(6 * 3);
+
+console.log("18 / 2 = ");
+console.log(18 / 2);
+
+console.log("4 to the 2nd power is: ");
+console.log(4 ** 2);
+
+console.log("The remainder when 25 is divided by 4 is: ");
+console.log(25 % 4);
+
+console.log("13 + 8 / 2 - 1 = ");
+console.log(13 + 8 / 2 - 1); //What will this equal? 16 (13 + 4 - 1)
+
+console.log("(13 + 8) / (2 - 1) = ");
+console.log((13 + 8) / (2 - 1)); //What will this equal? 21 (21 / 1) PEMDAS
+
+console.log("The sum of 9 and 15 is: ");
+console.log(9 + 15)
+console.log("The difference between 100 and 48 is: ");
+console.log(100 - 48)
+console.log("The product of 56 and 32 is: ");
+console.log(56 * 32)
+console.log("The quotient of 198 and 6 is: ");
+console.log(198 / 6);
+console.log("3 raised to the 5th power is: ");
+console.log(3 ** 5); 
+console.log("The remainder when 45 is divided by 7 is: ")
+console.log(45 % 7);
+console.log("The product of 5 and 7 added to the quotient of 12 and 3 is: ");
+console.log((5 * 7) + (12 / 3));
