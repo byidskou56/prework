9547cf1d1a3414f10e300da3a76b518d87e3ed59
byidskou56 <142067633+byidskou56@users.noreply.github.com>
2023-08-13 17:25:56 -0400

    Succesful Action Excercise 3

---
+++ Excercise 3.js
@@ -0,0 +1,11 @@
+//This is Excercise 3 of 16 PreWork BYI 13Aug23 17:25 EST
+console.log("This is a string, it has quotes.");
+console.log(100);
+console.log("100");
+
+console.log("My name is: ");
+console.log("Brandon Y Idskou"); //use string
+console.log("My age is: ");
+console.log(33)//use numerical
+console.log("My zip code is: "); 
+console.log("02910-4507"); //use string for address; numerical with cause equation actions
