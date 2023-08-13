6b9260eae2f73d26ec9825a49b61905353d45208
byidskou56 <142067633+byidskou56@users.noreply.github.com>
2023-08-13 18:33:05 -0400

    Successful Completion Exercise 5

---
+++ Excercise 5.js
@@ -0,0 +1,11 @@
+console.log("True or false: 5 is less than 2");
+console.log(5 < 2);
+console.log("True or false: 19 is not equal 11");
+console.log(19 != 11)
+console.log("True or false: 10 is greater than or equal to 10");
+console.log(10 >= 10);
+
+console.log(15 == 15); // ==, true
+console.log(2 > 4) // >, false
+console.log(29 <= 28); // <=, false
+console.log(23 != 20); // !=, true
